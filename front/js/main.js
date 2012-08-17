d186ebe5e7ba9a285e812ac169e126c90be39787
Nate Smith <nate.smith@coxinc.com>
2012-08-17 17:18:24 -0400

    fuck this shit

--- front/js/main.js
+++ front/js/main.js
@@ -10,4 +10,15 @@
         if (err) throw err
         cadigan.fetch(render)
     })
+
+    $('.navbar .nav > li > a.admin').click(function(e) {
+        $('.page').hide()
+        $('div#admin').show()
+        $('#workspace').html(
+    })
+
+    $('#controls a').click(function(e) {
+        var target = $(e.target).attr('data-target')
+        $('#'+target).show()
+    })
 })()
