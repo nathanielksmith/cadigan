2ebb3ac1d4d71efee902b033816f74c71fedee75
nathaniel k smith <nathanielksmith@gmail.com>
2012-09-17 16:45:24 -0400

    add test case stubs

---
+++ test/test_cli.js
@@ -0,0 +1,17 @@
+var cli = require('../lib/cli')
+var akeley = require('akeley')
+
+var test_new = {
+}
+
+var test_edit = {
+}
+
+var test_search_and_list = {
+}
+
+var test_publish = {
+}
+
+var test_delet = {
+}
